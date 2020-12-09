e5b99dadbcc7efeae3c37301c107b2144e3fed9c
Gracjan <graku12@gmail.com>
2020-12-09 22:25:30 +0100

    Init to develop

---
+++ src/Veles.Domain/Repository/IRefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Veles.Domain.Repository
+{
+   using System.Threading.Tasks;
+   using Veles.Domain.Entities;
+
+   public interface IRefreshTokenRepository
+   {
+      Task AddAsync(RefreshToken refreshToken);
+      Task<RefreshToken> GetAsync(string token);
+      Task UpdateAsync(RefreshToken refreshToken);
+   }
+}
