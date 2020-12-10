3a76e252be6477864ebebb5e8e2915737b513758
Gracjan <graku12@gmail.com>
2020-12-10 18:17:50 +0100

    feat(code): add TokenStorage

---
+++ src/Veles.Application/Authentication/Interfaces/ITokenStorage.cs
@@ -0,0 +1,12 @@
+﻿namespace Veles.Application.Authentication.Interfaces
+{
+   using System;
+   using Microsoft.Net.Http.Headers;
+   using Veles.Application.DTO;
+
+   public interface ITokenStorage
+   {
+      void Set(Guid commandId, AuthDto token);
+      AuthDto Get(Guid commandId);
+   }
+}
